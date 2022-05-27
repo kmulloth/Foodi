ad01355e53a322281eb079e38c2c83a3b3203f33
KMulloth <k.mulloth@gmail.com>
2022-05-27 12:44:41 -0500

    init

---
+++ frontend/src/store/index.js
@@ -0,0 +1,36 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { restoreCSRF, csrfFetch } from './csrf';
+import sessionReducer from './session';
+import * as sessionActions from './session';
+
+const rootReducer = combineReducers({
+  session: sessionReducer
+});
+
+let enhancer;
+
+if (process.env.NODE_ENV === 'production') {
+    enhancer = applyMiddleware(thunk);
+} else {
+    const logger = require('redux-logger').default;
+    const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+};
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+    restoreCSRF();
+
+    window.csrfFetch = csrfFetch;
+    window.store = store;
+    window.sessionActions = sessionActions;
+  }
+
+export default configureStore;
