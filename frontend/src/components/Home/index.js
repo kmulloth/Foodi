1bb9c130360fdf3ea0883beaf90c14e168007f01
KMulloth <k.mulloth@gmail.com>
2022-05-28 12:55:10 -0500

    styling

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './Home.css';
 
 function Home () {
+    return (
+        <div className="content">
+            <div className="businesses">
+                <h1>Businesses</h1>
+                <div className="business-list">
 
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
