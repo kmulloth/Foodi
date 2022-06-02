3d6ccfc85a21a2d79b07b3a5b77adcd34a0b0ad1
KMulloth <k.mulloth@gmail.com>
2022-06-01 21:14:28 -0500

    home page styling progress

--- frontend/src/components/Home/ImgCarousel.js
+++ frontend/src/components/Home/ImgCarousel.js
@@ -4,7 +4,7 @@ function ImgCarousel () {
         <div className="carousel-container">
             <div className="carousel-wrapper">
                 <div className="carousel-item">
-                    <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
+                    <img src="/images/foodtruck.jpg" alt="food truck" />
                 </div>
             </div>
         </div>
