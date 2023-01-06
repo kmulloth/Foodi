001cdc2c78cca9778d79c3928f14a454623b4d01
KMulloth <k.mulloth@gmail.com>
2023-01-05 19:13:05 -0500

    render deployment

--- frontend/src/components/BusinessForm/index.js
+++ frontend/src/components/BusinessForm/index.js
@@ -57,81 +57,83 @@ function BusinessForm() {
       <div className="business-form">
         <div className="business-form-container">
             <form id='submit-business-form' onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor="name">Title</label>
-                <input type="text" name="name" id="name" value={name} onChange={e => setName(e.target.value)} />
-              </div>
-              <div>
-                <label htmlFor="imgUrl">Cover Image (URL)</label>
-                <input type="text" name="imgUrl" id="imgUrl" value={imgUrl} onChange={e => setImgUrl(e.target.value)} />
-              </div>
-              <div>
-                <label htmlFor='type'>Establishment Type</label>
-                <select name='type' id='type' value={type} onChange={e => setType(e.target.value)}>
-                  <option name='restaurant' value='Restaurant'>Restaurant</option>
-                  <option name='truck' value='truck'>Food Truck</option>
-                  <option name='bar' value='Bar'>Bar</option>
-                </select>
-              </div>
-              <div>
-                <label htmlFor='cusine'>Cusine</label>
-                <select name='cusine' value={cusine} onChange={e => setCusine(e.target.value)}>
-                  <option value='American'>American</option>
-                  <option value='Japanese'>Japanese</option>
-                  <option value='French'>French</option>
-                  <option value='Italian'>Italian</option>
-                  <option value='Mediterranean'>Mediterranean</option>
-                </select>
-              </div>
-              <div>
-                <label htmlFor="location">Location</label>
-                <input type="text" name="location" id="location" disabled={true} value={location} onChange={e => setLocation(e.target.value)} />
-                <div id='mapform-container'>
-                  <MapForm lat={lat} setLat={setLat} lng={lng} setLng={setLng} setLocation={setLocation}/>
+              <div className='inputfields'>
+                <div className='textfields'>
+                  <div>
+                    <input placeholder='Title' type="text" name="name" id="name" value={name} onChange={e => setName(e.target.value)} />
+                  </div>
+                  <div>
+                    <input placeholder='Paste Image URL' type="text" name="imgUrl" id="imgUrl" value={imgUrl} onChange={e => setImgUrl(e.target.value)} />
+                  </div>
+                  <div>
+                  <input placeholder='Click on map to set Location' type="text" name="location" id="location" disabled={true} value={location} onChange={e => setLocation(e.target.value)} />
+                  </div>
+                  <div>
+                    <textarea placeholder='Describe your business' name="body" id="body" value={body} onChange={e => setBody(e.target.value)} />
+                  </div>
+                  <div>
+                    <label htmlFor='type'>Establishment Type</label>
+                    <select name='type' id='type' value={type} onChange={e => setType(e.target.value)}>
+                      <option name='restaurant' value='Restaurant'>Restaurant</option>
+                      <option name='truck' value='truck'>Food Truck</option>
+                      <option name='bar' value='Bar'>Bar</option>
+                    </select>
+                  </div>
+                  <div>
+                    <label htmlFor='cusine'>Cusine</label>
+                    <select name='cusine' value={cusine} onChange={e => setCusine(e.target.value)}>
+                      <option value='American'>American</option>
+                      <option value='Japanese'>Japanese</option>
+                      <option value='French'>French</option>
+                      <option value='Italian'>Italian</option>
+                      <option value='Mediterranean'>Mediterranean</option>
+                    </select>
+                  </div>
+                  <div>
+                    <label htmlFor="schedule">Hours</label>
+                    <div id='schedule-fields'>
+                        <div className='schedule-field'>
+                          <label htmlFor="schedule-0">S</label>
+                          <input type="time" name="schedule-0-open" id="schedule-0-open" value={openTimes[0]} onChange={e => setOpenTimes([ e.target.value, ...openTimes.slice(1)])} />
+                          <input type="time" name="schedule-0-close" id="schedule-0-close" value={closeTimes[0]} onChange={e => setCloseTimes([ e.target.value, ...closeTimes.slice(1)])} />
+                        </div>
+                        <div className='schedule-field'>
+                          <label htmlFor="schedule-0">M</label>
+                          <input type="time" name="schedule-1-open" id="schedule-1-open" value={openTimes[1]} onChange={e => setOpenTimes([ openTimes[0], e.target.value, ...openTimes.slice(2)])} />
+                          <input type="time" name="schedule-1-close" id="schedule-1-close" value={closeTimes[1]} onChange={e => setCloseTimes([closeTimes[0], e.target.value, ...closeTimes.slice(2)])}/>
+                        </div>
+                        <div className='schedule-field'>
+                          <label htmlFor="schedule-0">T</label>
+                          <input type="time" name="schedule-2-open" id="schedule-2-open" value={openTimes[2]} onChange={e => setOpenTimes([ ...openTimes.slice(0, 2), e.target.value, ...openTimes.slice(3)])}/>
+                          <input type="time" name="schedule-2-close" id="schedule-2-close" value={closeTimes[2]} onChange={e => setCloseTimes([ ...closeTimes.slice(0, 2), e.target.value, ...closeTimes.slice(3)])}/>
+                        </div>
+                        <div className='schedule-field'>
+                          <label htmlFor="schedule-0">W</label>
+                          <input type="time" name="schedule-3-open" id="schedule-3-open" value={openTimes[3]} onChange={e => setOpenTimes([...openTimes.slice(0, 3), e.target.value, ...openTimes.slice(4)])}/>
+                          <input type="time" name="schedule-3-close" id="schedule-3-close" value={closeTimes[3]} onChange={e => setCloseTimes([...closeTimes.slice(0, 3), e.target.value, ...closeTimes.slice(4)])}/>
+                        </div>
+                        <div className='schedule-field'>
+                          <label htmlFor="schedule-0">T</label>
+                          <input type="time" name="schedule-4-open" id="schedule-4-open" value={openTimes[4]} onChange={e => setOpenTimes([ ...openTimes.slice(0, 4), e.target.value, ...openTimes.slice(5)])}/>
+                          <input type="time" name="schedule-4-close" id="schedule-4-close" value={closeTimes[4]} onChange={e => setCloseTimes([...closeTimes.slice(0, 4), e.target.value, ...closeTimes.slice(5)])}/>
+                        </div>
+                        <div className='schedule-field'>
+                          <label htmlFor="schedule-0">F</label>
+                          <input type="time" name="schedule-5-open" id="schedule-5-open" value={openTimes[5]} onChange={e => setOpenTimes([...openTimes.slice(0, 5) ,e.target.value, openTimes[6]])}/>
+                          <input type="time" name="schedule-5-close" id="schedule-5-close" value={closeTimes[5]} onChange={e => setCloseTimes([...closeTimes.slice(0, 5), e.target.value, closeTimes[6]])}/>
+                        </div>
+                        <div className='schedule-field'>
+                          <label htmlFor="schedule-0">S</label>
+                          <input type="time" name="schedule-6-open" id="schedule-6-open" value={openTimes[6]} onChange={e => setOpenTimes([...openTimes.splice(0, 6), e.target.value])}/>
+                          <input type="time" name="schedule-6-close" id="schedule-6-close" value={closeTimes[6]} onChange={e => setCloseTimes([...closeTimes.splice(0, 6), e.target.value])}/>
+                        </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <label htmlFor="body">Body</label>
-                <textarea name="body" id="body" value={body} onChange={e => setBody(e.target.value)} />
-              </div>
-              <div>
-                <label htmlFor="schedule">Hours</label>
-                <div id='schedule-fields'>
-                  <label htmlFor="schedule-0">Sunday</label>
-                    <div className='schedule-field'>
-                      <input type="time" name="schedule-0-open" id="schedule-0-open" value={openTimes[0]} onChange={e => setOpenTimes([ e.target.value, ...openTimes.slice(1)])} />
-                      <input type="time" name="schedule-0-close" id="schedule-0-close" value={closeTimes[0]} onChange={e => setCloseTimes([ e.target.value, ...closeTimes.slice(1)])} />
-                    </div>
-                  <label htmlFor="schedule-0">Monday</label>
-                    <div className='schedule-field'>
-                      <input type="time" name="schedule-1-open" id="schedule-1-open" value={openTimes[1]} onChange={e => setOpenTimes([ openTimes[0], e.target.value, ...openTimes.slice(2)])} />
-                      <input type="time" name="schedule-1-close" id="schedule-1-close" value={closeTimes[1]} onChange={e => setCloseTimes([closeTimes[0], e.target.value, ...closeTimes.slice(2)])}/>
-                    </div>
-                  <label htmlFor="schedule-0">Tuesday</label>
-                    <div className='schedule-field'>
-                      <input type="time" name="schedule-2-open" id="schedule-2-open" value={openTimes[2]} onChange={e => setOpenTimes([ ...openTimes.slice(0, 2), e.target.value, ...openTimes.slice(3)])}/>
-                      <input type="time" name="schedule-2-close" id="schedule-2-close" value={closeTimes[2]} onChange={e => setCloseTimes([ ...closeTimes.slice(0, 2), e.target.value, ...closeTimes.slice(3)])}/>
-                    </div>
-                  <label htmlFor="schedule-0">Wednesday</label>
-                    <div className='schedule-field'>
-                      <input type="time" name="schedule-3-open" id="schedule-3-open" value={openTimes[3]} onChange={e => setOpenTimes([...openTimes.slice(0, 3), e.target.value, ...openTimes.slice(4)])}/>
-                      <input type="time" name="schedule-3-close" id="schedule-3-close" value={closeTimes[3]} onChange={e => setCloseTimes([...closeTimes.slice(0, 3), e.target.value, ...closeTimes.slice(4)])}/>
-                    </div>
-                  <label htmlFor="schedule-0">Thursday</label>
-                    <div className='schedule-field'>
-                      <input type="time" name="schedule-4-open" id="schedule-4-open" value={openTimes[4]} onChange={e => setOpenTimes([ ...openTimes.slice(0, 4), e.target.value, ...openTimes.slice(5)])}/>
-                      <input type="time" name="schedule-4-close" id="schedule-4-close" value={closeTimes[4]} onChange={e => setCloseTimes([...closeTimes.slice(0, 4), e.target.value, ...closeTimes.slice(5)])}/>
-                    </div>
-                  <label htmlFor="schedule-0">Friday</label>
-                    <div className='schedule-field'>
-                      <input type="time" name="schedule-5-open" id="schedule-5-open" value={openTimes[5]} onChange={e => setOpenTimes([...openTimes.slice(0, 5) ,e.target.value, openTimes[6]])}/>
-                      <input type="time" name="schedule-5-close" id="schedule-5-close" value={closeTimes[5]} onChange={e => setCloseTimes([...closeTimes.slice(0, 5), e.target.value, closeTimes[6]])}/>
-                    </div>
-                  <label htmlFor="schedule-0">Saturday</label>
-                    <div className='schedule-field'>
-                      <input type="time" name="schedule-6-open" id="schedule-6-open" value={openTimes[6]} onChange={e => setOpenTimes([...openTimes.splice(0, 6), e.target.value])}/>
-                      <input type="time" name="schedule-6-close" id="schedule-6-close" value={closeTimes[6]} onChange={e => setCloseTimes([...closeTimes.splice(0, 6), e.target.value])}/>
-                    </div>
+                <div className='mapfield'>
+                  <div id='mapform-container'>
+                    <MapForm lat={lat} setLat={setLat} lng={lng} setLng={setLng} setLocation={setLocation}/>
+                  </div>
                 </div>
               </div>
               <button type="submit" disabled={errors.length > 0}>Submit</button>
--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import { NavLink } from 'react-router-dom';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
@@ -13,6 +13,7 @@ const center = {
 };
 
 function Map({businesses}) {
+  const [selectedBiz, setSelectedBiz] = useState()
   const [key, setKey] = useState()
 
   useEffect(() => {
@@ -31,14 +32,34 @@ function Map({businesses}) {
         mapContainerStyle={containerStyle}
         center={center}
         zoom={10}
+        onClick={() => setSelectedBiz(null)}
       >
         { Object.values(businesses).map(business => (
           <Marker
               key={business?.id}
               position={{lat: business?.lat, lng: business?.lng}}
+              onClick={() => setSelectedBiz(business)}
             />
-          ))}
-        <></>
+        ))}
+        {selectedBiz && (
+          <InfoWindow
+            position={{lat: selectedBiz.lat, lng: selectedBiz.lng}}
+            onCloseClick={() => setSelectedBiz(null)}
+          >
+            <div className='info-window'>
+              <NavLink to={`/businesses/${selectedBiz.id}`}>
+                <img src={selectedBiz.imgUrl} alt={selectedBiz.name} />
+                <h3>{selectedBiz.name}</h3>
+                <p>{selectedBiz.cusine} {selectedBiz.type === 'truck' ? 'Food Truck' : selectedBiz.type}</p>
+                <div
+                    className="Stars"
+                    style={{'--rating': selectedBiz?.rating.toFixed(1)}}
+                    aria-label={`Rating of this selectedBiz is ${selectedBiz?.rating / 5 * 100}%`}
+                />
+              </NavLink>
+            </div>
+          </InfoWindow>
+        )}
       </GoogleMap>
     </LoadScript>
   )
